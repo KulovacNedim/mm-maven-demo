8979133b680db244c769396ef2232a9725360e88
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-02 20:46:55 +0100

    web-app module added. vUserController with a fake implementation added. dependencies on jpa-enteties and web-api included

---
+++ web-app/src/main/java/dev/ned/controllers/UserController.java
@@ -0,0 +1,13 @@
+package dev.ned.controllers;
+
+import dev.ned.domain.UserCommand;
+import dev.ned.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // fake impl
+        return new User();
+    }
+}
